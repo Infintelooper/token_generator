90d36a3d67f90ef20442161aaf80ba333878520a
soorajkrishnan <krishnanvssooraj@gmail.com>
2020-08-17 14:02:23 +0530

    added fragments

--- app/src/main/java/com/cleancodec/tokengenerator/MainActivity.kt
+++ app/src/main/java/com/cleancodec/tokengenerator/MainActivity.kt
@@ -3,12 +3,16 @@ package com.cleancodec.tokengenerator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.cleancodec.tokengenerator.fragment.clock
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val clockFragment= clock()
+
+        bottom
 
     }
 
