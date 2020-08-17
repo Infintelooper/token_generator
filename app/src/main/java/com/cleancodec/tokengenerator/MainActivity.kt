548e087562b28730f9026464493b9e9b39ea4a51
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2020-08-17 21:23:44 +0530

    movement translation from home to clock

--- app/src/main/java/com/cleancodec/tokengenerator/MainActivity.kt
+++ app/src/main/java/com/cleancodec/tokengenerator/MainActivity.kt
@@ -2,24 +2,24 @@ package com.cleancodec.tokengenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 import com.cleancodec.tokengenerator.fragment.clock
 import com.cleancodec.tokengenerator.fragment.home
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val clockFragment= clock()
+        val homeFragment = home()
 
-
+        makeCurrentFragment(homeFragment)
         home_icon.setOnClickListener(){
-            //Log.i("MyClass.getView() — get item number", "MyClass.getView() — get item number")
+            makeCurrentFragment(clockFragment)
 
-        //val clockFragment= clock()
 
 
         }
@@ -28,9 +28,14 @@ class MainActivity : AppCompatActivity() {
     private fun makeCurrentFragment(fragment: Fragment)
     {
         supportFragmentManager.beginTransaction().apply {
+        setCustomAnimations(R.animator.slide_in_left,
+                            R.animator.slide_out_right, 0, 0)
             replace(R.id.fl_wrapper,fragment)
             commit()
         }
+
+
+
     }
     }
 
