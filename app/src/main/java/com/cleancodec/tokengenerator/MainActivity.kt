7b9f9cbd7e6f09d0e516437531d2b82c88908e62
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2020-08-17 19:37:22 +0530

    added onclickListener in MainActivity.kt

--- app/src/main/java/com/cleancodec/tokengenerator/MainActivity.kt
+++ app/src/main/java/com/cleancodec/tokengenerator/MainActivity.kt
@@ -2,17 +2,22 @@ package com.cleancodec.tokengenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
 import com.cleancodec.tokengenerator.fragment.clock
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        home_icon.setOnClickListener(){
+            Log.i("MyClass.getView() — get item number", "MyClass.getView() — get item number")
 
         val clockFragment= clock()
 
+        }
 
     }
-
     }
+
