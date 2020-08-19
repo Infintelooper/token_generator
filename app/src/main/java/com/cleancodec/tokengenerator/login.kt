d4b488cb2ebbfb304cbdd0c783f1182a6608e32b
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2020-08-19 22:25:32 +0530

    added some shadow effects and a login activity_login.xml

---
+++ app/src/main/java/com/cleancodec/tokengenerator/login.kt
@@ -0,0 +1,11 @@
+package com.cleancodec.tokengenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
