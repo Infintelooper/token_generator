c1e3e533dd1d114ba0af169992f653c1b88a3b12
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2020-08-20 23:38:32 +0530

    loading page after 5 sec move to home page

--- app/src/main/java/com/cleancodec/tokengenerator/login.kt
+++ app/src/main/java/com/cleancodec/tokengenerator/login.kt
@@ -1,7 +1,10 @@
 package com.cleancodec.tokengenerator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import kotlinx.android.synthetic.main.activity_login.*
 
 class login : AppCompatActivity() {
@@ -13,6 +16,17 @@ class login : AppCompatActivity() {
 
         login_button.setOnClickListener {
             loadingDialog.startLoadingDialog()
+            Handler(Looper.getMainLooper()).postDelayed({
+                successLogin()
+                loadingDialog.dissmissDialog()     /// need to add changes on OTP confirmation
+
+            }, 5000)
         }
+    }
+    fun successLogin()
+    {
+        val intent = Intent(this, MainActivity::class.java)
+        // start your next activity
+        startActivity(intent)
     }
 }
