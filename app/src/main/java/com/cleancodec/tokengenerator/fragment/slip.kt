db9b91097f78e243eaa796a694742026ce224842
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2020-08-23 21:37:59 +0530

    token setup version 5 with no error, added OnListerer to onViewCreated

--- app/src/main/java/com/cleancodec/tokengenerator/fragment/slip.kt
+++ app/src/main/java/com/cleancodec/tokengenerator/fragment/slip.kt
@@ -1,6 +1,7 @@
 package com.cleancodec.tokengenerator.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -42,6 +43,12 @@ class slip : Fragment() {
         return inflater.inflate(R.layout.fragment_slip, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tap_message.setOnClickListener{
+            Log.i("Hai","There")
+        }
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
